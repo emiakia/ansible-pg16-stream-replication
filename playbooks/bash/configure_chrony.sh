dff2ee9e4ed7f8748505a37099e722665b900aaf
Emran Kia <emran.kia@gmail.com>
2024-09-04 19:17:19 +0200

    ubuntu after preinstallation

--- playbooks/bash/configure_chrony.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Configure Chrony
-sed -i "/^server /d" /etc/chrony.conf
-echo "server $1 iburst" >> /etc/chrony.conf
--- playbooks/bash/disable_selinux.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Disable SELinux
-sed -i 's/^SELINUX=.*/SELINUX=disabled/g' /etc/selinux/config
-setenforce 0
